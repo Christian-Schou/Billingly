3bef4db30d873e4a502fea5a9e92ce46c015095c
Christian Schou <christian.schou@hotmail.com>
2022-11-14 22:05:25 +0100

    Added Customer Service

---
+++ Data/Repositories/CustomerRepository.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq.Expressions;
+using Billingly.Data.Repositories.Base;
+using Billingly.Entities.Invoice;
+using Billingly.Services;
+using Billingly.WEB.Services;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+namespace Billingly.Data.Repositories
+{
+    public class CustomerRepository : BaseRepository<Customer>, ICustomerService
+    {
+        public CustomerRepository(ApplicationDbContext appDbContext)
+            : base(appDbContext)
+        {
+        }
+
+        public void CreateCustomer(Customer customer)
+        {
+            Create(customer);
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Delete(customer);
+        }
+
+        public async Task<IEnumerable<Customer>> GetAllCustomersAsync()
+        {
+            return await FindAll()
+                .OrderBy(customer => customer.Name)
+                .ToListAsync();
+        }
+
+        public IEnumerable<SelectListItem> GetAllCustomersForDropDown()
+        {
+            return FindAll().Select(customer => new SelectListItem
+            {
+                Text = customer.Name,
+                Value = customer.Id.ToString()
+            });
+        }
+
+
+        public async Task<Customer> GetCustomerByIdAsync(int customerId)
+        {
+            return await FindByCondition(customer => customer.Id.Equals(customerId))
+                .FirstOrDefaultAsync();
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            Update(customer);
+        }
+    }
+}
+
--- Services/ICustomerInterface.cs
+++ Services/ICustomerInterface.cs
@@ -2,12 +2,18 @@
 using Billingly.Entities.Invoice;
 using Billingly.Services;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using static Humanizer.In;
 
 namespace Billingly.WEB.Services
 {
     public interface ICustomerService : IBaseService<Customer>
     {
-        Task<IEnumerable<SelectListItem>> GetAllCustomersForDropDownAsync();
+        IEnumerable<SelectListItem> GetAllCustomersForDropDown();
+        Task<IEnumerable<Customer>> GetAllCustomersAsync();
+        Task<Customer> GetCustomerByIdAsync(int customerId);
+        void CreateCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);
     }
 }
 
