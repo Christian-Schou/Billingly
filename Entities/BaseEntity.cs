a63a383c461045d0d4d111ac0dd9a0541d49db41
Christian Schou <christian.schou@hotmail.com>
2022-11-14 20:31:10 +0100

    Project creation

---
+++ Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Billingly.Entities
+{
+    public class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Column(TypeName = "datetime2")]
+        public DateTime CreateDate { get; set; }
+        [Column(TypeName = "datetime2")]
+        public DateTime UpdateDate { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
+
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Billingly.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
