f52c43cfdf214d4df723fd7329dcaf160af8cf3c
Christian Schou <christian.schou@hotmail.com>
2022-11-14 21:15:42 +0100

    Added entities to database

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Billingly.Entities.Invoice;
+using Billingly.Entities.Application;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Billingly.Data;
@@ -9,5 +11,11 @@ public class ApplicationDbContext : IdentityDbContext
         : base(options)
     {
     }
+
+    public DbSet<SystemSettings> SystemSettings { get; set; }
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<InvoiceItem> InvoiceItems { get; set; }
+    public DbSet<Invoice> Invoices { get; set; }
 }
 
